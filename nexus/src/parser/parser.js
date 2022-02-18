108a59c094d379e5935db7c7d4c501bc9052e134
mykongee <mikehongde@gmail.com>
2022-02-17 17:40:51 -0800

    added parsing logic to get component and component props

--- nexus/src/parser/parser.js
+++ nexus/src/parser/parser.js
@@ -3,16 +3,109 @@ var jsx = require('acorn-jsx');
 var JSXPARSER = PARSER.extend(jsx());
 var fs = require('fs');
 var path = require('path');
+// CONSTANTS 
+var JSXTEXT = 'JSXText';
+var JSXELEMENT = 'JSXElement';
+var JSXEXPRESSIONCONTAINER = 'JSXExpressionContainer';
+var ComponentNode = /** @class */ (function () {
+    function ComponentNode(name, props, children) {
+        this.name = name;
+        this.children = children;
+        this.props = props;
+    }
+    return ComponentNode;
+}());
+var PropsNode = /** @class */ (function () {
+    function PropsNode(name, value) {
+        this.name = name;
+        this.value = value;
+    }
+    return PropsNode;
+}());
+// Parser
 var source = fs.readFileSync(path.resolve(__dirname, './App.jsx'));
 var parsed = JSXPARSER.parse(source, { sourceType: "module" });
-// console.log(parsed); // -> Program Node {type, start, end, body[]}
-var programBody = parsed.body; // get body of Program Node
+var programBody = parsed.body; // get body of Program Node(i.e. source code entry)
+// const variables that hold different nodes depending on type 
+var importNodes = programBody.filter(function (node) { return node.type === 'ImportDeclaration'; });
+var otherNodes = programBody.filter(function (node) { return node.type !== 'ImportDeclaration'; });
+var variableNodes = programBody.filter(function (node) { return node.type === 'VariableDeclaration'; });
+// find export default declaration to get entire component
+var exportDefaultNode = otherNodes.filter(function (node) {
+    node.type === 'ExportDefaultDeclaration';
+})[0];
+var returnChildren = variableNodes[variableNodes.length - 1].declarations[0].init.body.body[1].argument.children;
+var jsxNodes = returnChildren.filter(function (node) { return node.type === JSXELEMENT; });
+var components = [];
+for (var _i = 0, jsxNodes_1 = jsxNodes; _i < jsxNodes_1.length; _i++) {
+    var node = jsxNodes_1[_i];
+    var firstChar = node.openingElement.name.name[0]; // actual name label (i.e. 'Chatroom', 'Component')
+    if (firstChar === firstChar.toUpperCase()) {
+        var componentNode = new ComponentNode(node.openingElement.name.name, node.children);
+        // newNode.name = node.openingElement.name.name;
+        // newNode.props = findProps(node);
+        // newNode.children = node.children;
+        var props = getNodeProps(node);
+        componentNode.props = props;
+        components.push(componentNode);
+        // get name of component, props, children<boolean>
+        // console.log(node);
+    }
+}
+console.log(components);
+// Node {
+//   type: 'JSXElement',
+//   start: 815,
+//   end: 857,
+//   openingElement: Node {
+//     type: 'JSXOpeningElement',
+//     start: 815,
+//     end: 857,
+//     attributes: [ [Node], [Node] ],
+//     name: Node {
+//       type: 'JSXIdentifier',
+//       start: 816,
+//       end: 825,
+//       name: 'Chatrooms'
+//     },
+//     selfClosing: true
+//   },
+//   closingElement: null,
+//   children: []
+// }
 // some logic that will help filter out nodes that we actually need
 // use a switch statement to avoid too many if conditional statements
-function switchConditional(expression) {
-    // switch(expression){
-    //     case 
-    // }
+// <Chatroom name={'Brian'} otherProp={500}/>
+// value: Node {
+//   type: 'Literal',
+//   start: 650,
+//   end: 657,
+//   value: '/dogs',
+//   raw: '"/dogs"'
+// }
+// value: Node {
+//   type: 'JSXExpressionContainer',
+//   start: 775,
+//   end: 784,
+//   expression: [Node]
+// }
+function getPropValue(node) {
+    if (Object.keys(node).includes('expression')) {
+        return node.expression.value; // look into the value (node) and find the expression 
+    }
+    else {
+        return node.value; // return the value 
+    }
+}
+function getNodeProps(node) {
+    var propArr = [];
+    for (var _i = 0, _a = node.openingElement.attributes; _i < _a.length; _i++) {
+        var prop = _a[_i];
+        var newProps = new PropsNode(prop.name.name, getPropValue(prop.value));
+        propArr.push(newProps);
+    }
+    // console.log(propArr);
+    return propArr;
 }
 function findReturnedComponents(arr) {
     // loop through all
@@ -29,28 +122,8 @@ function findReturnedComponents(arr) {
     // findProps()
     // if it does, do something 
 }
-var JSXTEXT = 'JSXText';
-var JSXELEMENT = 'JSXElement';
-var JSXEXPRESSIONCONTAINER = 'JSXExpressionContainer';
 // findPropsChild(arr: Array<any>){
 // }
-var importNodes = programBody.filter(function (node) { return node.type === 'ImportDeclaration'; });
-var otherNodes = programBody.filter(function (node) { return node.type !== 'ImportDeclaration'; });
-var variableNodes = programBody.filter(function (node) { return node.type === 'VariableDeclaration'; });
-// find export default declaration to get entire component
-var exportDefaultNode = otherNodes.filter(function (node) {
-    node.type === 'ExportDefaultDeclaration';
-})[0];
-var returnChildren = variableNodes[variableNodes.length - 1].declarations[0].init.body.body[1].argument.children;
-var jsxNodes = returnChildren.filter(function (node) { return node.type === JSXELEMENT; });
-for (var _i = 0, jsxNodes_1 = jsxNodes; _i < jsxNodes_1.length; _i++) {
-    var node = jsxNodes_1[_i];
-    console.log(node);
-    var firstChar = node.openingElement.name.name[0]; // actual name label (i.e. 'Chatroom', 'Component')
-    if (firstChar === firstChar.toUpperCase()) {
-        // console.log(node);
-    }
-}
 // console.log(exportDefaultNode.declaration.properties);
 // iterate through other nodes to find children components
 // othernodes -> find type: VariableDeclaration where there is return statement
--- nexus/src/parser/parser.ts
+++ nexus/src/parser/parser.ts
@@ -4,6 +4,12 @@ const JSXPARSER = PARSER.extend(jsx());
 const fs = require('fs');
 const path = require('path');
 
+// CONSTANTS 
+const JSXTEXT: string = 'JSXText';
+const JSXELEMENT: string = 'JSXElement';
+const JSXEXPRESSIONCONTAINER: string = 'JSXExpressionContainer';
+
+// TS Interface
 interface Node {
   type: string,
   start: number,
@@ -15,25 +21,135 @@ interface Node {
   properties: Array<Node>,
   method: boolean,
   init: Node,
-  body: Array<any>|Object,
+  body: Array<any>|object,
   children: Array<Node>,
   argument: Node,
-  openingElement: Node
+  openingElement: Node,
+  name: Node|string,
+  attributes: Array<Node>,
+  props: Node,
+  expression: Node,
 }
 
-const source = fs.readFileSync(path.resolve(__dirname, './App.jsx'));
-const parsed = JSXPARSER.parse(source, {sourceType: "module"});
-// console.log(parsed); // -> Program Node {type, start, end, body[]}
-const programBody: Array<Node> = parsed.body; // get body of Program Node
+interface ComponentNode {
+  name: string,
+  children: Array<any>,
+  props: Array<PropsNode>
+}
 
+class ComponentNode {
+  constructor(name: string, props: Array<PropsNode>, children: Array<any>) {
+    this.name = name;
+    this.children = children;
+    this.props = props;
+  }
+}
+
+interface PropsNode {
+  props: Node,
+  name: String,
+  value: any,
+}
+
+class PropsNode {
+  constructor(name:String,value:any){
+    this.name = name;
+    this.value = value;
+  }
+}
+// Parser
+const source = fs.readFileSync(path.resolve(__dirname, './App.jsx'));
+const parsed = JSXPARSER.parse(source, {sourceType: "module"}); 
+const programBody: Array<Node> = parsed.body; // get body of Program Node(i.e. source code entry)
+
+// const variables that hold different nodes depending on type 
+const importNodes: Array<Node> = programBody.filter((node: Node) => node.type === 'ImportDeclaration');
+const otherNodes: Array<Node> = programBody.filter((node: Node) => node.type !== 'ImportDeclaration');
+const variableNodes: Array<Node> = programBody.filter((node: Node) => node.type === 'VariableDeclaration');
+
+
+// find export default declaration to get entire component
+const exportDefaultNode: Node = otherNodes.filter((node: Node) => {
+  node.type === 'ExportDefaultDeclaration';
+})[0];
+
+const returnChildren = variableNodes[variableNodes.length-1].declarations[0].init.body.body[1].argument.children;
+const jsxNodes: Array<Node> = returnChildren.filter((node: Node) => node.type === JSXELEMENT);
+
+
+const components = [];
+for (let node of jsxNodes) {
+  const firstChar = node.openingElement.name.name[0]; // actual name label (i.e. 'Chatroom', 'Component')
+  if (firstChar === firstChar.toUpperCase()) {
+    const componentNode = new ComponentNode(node.openingElement.name.name, node.children);
+    // newNode.name = node.openingElement.name.name;
+    // newNode.props = findProps(node);
+    // newNode.children = node.children;
+    const props = getNodeProps(node);
+    componentNode.props = props;
+    components.push(componentNode);
+    // get name of component, props, children<boolean>
+    // console.log(node);
+    
+  }
+}
+console.log(components);
+// Node {
+//   type: 'JSXElement',
+//   start: 815,
+//   end: 857,
+//   openingElement: Node {
+//     type: 'JSXOpeningElement',
+//     start: 815,
+//     end: 857,
+//     attributes: [ [Node], [Node] ],
+//     name: Node {
+//       type: 'JSXIdentifier',
+//       start: 816,
+//       end: 825,
+//       name: 'Chatrooms'
+//     },
+//     selfClosing: true
+//   },
+//   closingElement: null,
+//   children: []
+// }
 // some logic that will help filter out nodes that we actually need
     // use a switch statement to avoid too many if conditional statements
-function switchConditional(expression:Array<Node>){
-    // switch(expression){
-    //     case 
-    // }
+// <Chatroom name={'Brian'} otherProp={500}/>
+
+// value: Node {
+//   type: 'Literal',
+//   start: 650,
+//   end: 657,
+//   value: '/dogs',
+//   raw: '"/dogs"'
+// }
+
+// value: Node {
+//   type: 'JSXExpressionContainer',
+//   start: 775,
+//   end: 784,
+//   expression: [Node]
+// }
+
+function getPropValue(node: Node) {
+  if (Object.keys(node).includes('expression')) {
+    return node.expression.value; // look into the value (node) and find the expression 
+  } else {
+    return node.value; // return the value 
+  }
 }
 
+function getNodeProps(node:Node){
+  const propArr: Array<any> = [];
+  for(let prop of node.openingElement.attributes){
+    const newProps = new PropsNode(prop.name.name, getPropValue(prop.value));
+    propArr.push(newProps);
+  }
+  // console.log(propArr);
+  return propArr;
+}
 function findReturnedComponents(arr: Array<Node>) {
   // loop through all
   // look inside each node to see if it returns jsxcomponents
@@ -50,31 +166,13 @@ function findReturnedComponents(arr: Array<Node>) {
   // if it does, do something 
 }
 
-const JSXTEXT: string = 'JSXText';
-const JSXELEMENT: string = 'JSXElement';
-const JSXEXPRESSIONCONTAINER: string = 'JSXExpressionContainer';
 
 
 // findPropsChild(arr: Array<any>){
     
 // }
 
-const importNodes: Array<Node> = programBody.filter((node: Node) => node.type === 'ImportDeclaration');
-const otherNodes: Array<Node> = programBody.filter((node: Node) => node.type !== 'ImportDeclaration');
-const variableNodes: Array<Node> = programBody.filter((node: Node) => node.type === 'VariableDeclaration');
-// find export default declaration to get entire component
-const exportDefaultNode: Node = otherNodes.filter((node: Node) => {
-  node.type === 'ExportDefaultDeclaration';
-})[0];
 
-const returnChildren = variableNodes[variableNodes.length-1].declarations[0].init.body.body[1].argument.children;
-const jsxNodes: Array<Node> = returnChildren.filter((node: Node) => node.type === JSXELEMENT);
-for (let node of jsxNodes) {
-  const firstChar = node.openingElement.name.name[0]; // actual name label (i.e. 'Chatroom', 'Component')
-  if(firstChar === firstChar.toUpperCase()){
-    console.log(node);
-  }
-}
 // console.log(exportDefaultNode.declaration.properties);
 
 // iterate through other nodes to find children components

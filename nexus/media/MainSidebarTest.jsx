25dad030993a9e92aff7012a756faa0b2edcf65d
Alex Hersler <ahersler@gmail.com>
2022-02-28 19:58:19 -0800

    add more details to logs in mainsidebartest

--- nexus/media/MainSidebarTest.jsx
+++ nexus/media/MainSidebarTest.jsx
@@ -18,58 +18,47 @@ class MainSideBarTest extends Component {
   constructor() {
     super();
     this.state = {
-      data: {}
+      data: {},
     };
-    
   }
 
   componentDidMount() {
     console.log('in component did mount');
     // add event listner listening for message
-    window.addEventListener('message', (event) => {
+    window.addEventListener('message', event => {
       console.log(event);
-      this.setState((prevState) => ({
-          ...prevState,
-          data: event.data,
-          // name: this.state.data.children[0].name;
-        }));
+      this.setState(prevState => ({
+        ...prevState,
+        data: event.data,
+        // name: this.state.data.children[0].name;
+      }));
       // cb function --=>
-        // grab data and update state
-    
-  });
-};
-
-
+      // grab data and update state
+    });
+  }
 
   render() {
     // res.map(// factory make Node components from data)
 
-
     let children;
 
     if (this.state.data.name) {
-      
-    console.log('state.data.children', this.state.data.children);
+      console.log('state.data.children', this.state.data.children);
 
-      children = this.state.data.children.map((child) => {
+      children = this.state.data.children.map(child => {
         if (child.children.length) {
-          console.log('making a nodewithchildren');
-          return <NodeWithChildren data={child} />;}
-        else {
-          console.log('making a leaf');
-          return <Leaf data={child} />;}
-    });
-  }
+          console.log('making a nodewithchildren', child.name);
+          return <NodeWithChildren data={child} />;
+        } else {
+          console.log('making a leaf', child.name);
+          return <Leaf data={child} />;
+        }
+      });
+    }
 
+    console.log('children array', children);
 
-  console.log('children array', children);  
-  
-    
-    return (
-      <div>
-        {children || <p>No Data in State</p>}
-      </div>
-    );
+    return <div>{children || <p>No Data in State</p>}</div>;
   }
 }
 

1c0300b11ca46b2d39c79c1001e5e811e4fe422e
mykongee <mikehongde@gmail.com>
2022-03-03 10:30:22 -0800

    Moved webview components out of media folder into src/webview

--- nexus/webpack.config.js
+++ nexus/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './media/renderToDom.js'),
+  entry: path.resolve(__dirname, './src/webview/renderToDom.js'),
   mode: 'production',
   target: 'node',
   externals: {

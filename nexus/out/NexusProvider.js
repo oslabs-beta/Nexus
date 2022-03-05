f835888b096d7a75776ca2c8ae3457e4c14bf48c
nicoflrs <floresni1996@gmail.com>
2022-03-05 15:21:18 -0800

    Adapted the provider to work with the WSL and Windows directories.
    Co-authored-by: Alex Hersler <alex@alexhersler.com>

--- nexus/out/NexusProvider.js
+++ nexus/out/NexusProvider.js
@@ -27,9 +27,43 @@ class NexusProvider {
     // send message to webviewAPI with data using webview.postMessage(data)
     parseCodeBaseAndSendMessage(filePath) {
         console.log('dirname: ', __dirname);
-        console.log('path.resolve: ', path.resolve(__dirname, filePath));
-        // const resultObj = new Parser(fs.readFileSync(path.resolve(__dirname, filePath))); // --> works
-        const resultObj = new parser_js_1.Parser(fs.readFileSync(path.resolve(__dirname, './parser/newApp.jsx'))); // -> works
+        console.log('passed-in filepath: ', filePath);
+        console.log('path.resolve hardcoded: ', path.resolve(__dirname, './parser/App.jsx'));
+        // const resultObj = new Parser(fs.readFileSync('mnt/c/C:\\Users\\Nico\\Desktop\\nexus-copy\\out\\parser\\App.jsx')); // --> works //path.resolve:   
+        //passed-in filepath:  C:\Users\Nico\Desktop\nexus-copy\out\parser\App.jsx
+        let str = filePath;
+        // let str;
+        if (process.platform === 'linux') {
+            if (/wsl\$/.test(filePath)) {
+                // filePath = // -> \\wsl$\Ubuntu-20.04\home\nicoflo\unit-6-react-tic-tac-toe\src\app.jsx
+                str = '/home' + filePath.split('home')[1].replace(/\\/g, '/');
+                console.log(str);
+                /*
+                        str = path.resolve(filePath.replace(/\\/g, '/'));
+                        console.log('wsl str 1: ', str); // ->  /wsl$/Ubuntu-20.04/home/nicoflo/unit-6-react-tic-tac-toe/src/app.jsx
+                
+                        
+                
+                        str = '/' + str.split('/').slice(3).join('/');
+                        console.log('wsl str 2: ', str); // -> /home/nicoflo/unit-6-react-tic-tac-toe/src/app.jsx
+                */
+                /*
+            
+              this.entryFile = '/' + this.entryFile.split('/').slice(3).join('/');
+              */
+            }
+            else {
+                str = '/mnt/c/' + filePath.slice(3);
+                str = str.replace(/\\/g, '/');
+            }
+        }
+        console.log(str);
+        // \\wsl$\
+        console.log(path.win32.sep);
+        console.log(path.posix.sep);
+        const resultObj = new parser_js_1.Parser(fs.readFileSync(str)); // --> works //path.resolve:   
+        // const resultObj = new Parser(fs.readFileSync('/mnt/c/Users/Nico/Desktop/nexus-copy/out/parser/App.jsx')); // --> works //path.resolve:   
+        // const resultObj = new Parser(fs.readFileSync(path.resolve(__dirname, './parser/App.jsx'))); // -> works      
         // const resultObj = new Parser(fs.readFileSync(path.resolve(__dirname, '/Users/davidlee/Nexus/nexus/src/parser/newApp.jsx'))); // -> works
         const data = resultObj.main();
         console.log('FUNCTIONAL NODES: ', data);

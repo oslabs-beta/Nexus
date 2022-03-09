ff5a03c7fa97ea2299d41bc77dc3cd37161fba23
mykongee <mikehongde@gmail.com>
2022-03-09 00:48:10 -0800

    Completed recursive logic in parser to grab nodes specified by useRouter.
    Co-authored-by: Nico Flores <floresni1996@gmail.com>

--- nexus/src/parser/parserv2.ts
+++ nexus/src/parser/parserv2.ts
@@ -7,6 +7,7 @@ import * as jsx from 'acorn-jsx';
 const JSXPARSER = PARSER.extend(jsx());
 import * as fs from 'fs';
 import * as path from 'path';
+import { kill } from 'process';
 
 // const fs = require("fs");
 // const path = require("path");
@@ -156,10 +157,13 @@ export class Parser {
     const returnStatement = exportObject.declaration.body.body[exportObject.declaration.body.body.length-1];
     const jsxElements = this.getJsxNodes(returnStatement.argument.children); //JSXElement
     const endpoints = [];
+    // console.log('returnStatement: ', returnStatement)
+    // console.log('jsxElements: ', jsxElements)
     // TODO: change from hardcoded 0th index
     // iterate through jsxelements, find node with router.push() callExpression
       // for each jsxelement, look at children and filter jsxelements again
       const nestedJsxElements = this.getJsxNodes(jsxElements[0].children);
+      // console.log('nestedJsxElements: ', nestedJsxElements);
         // then, for each jsxelement, look at JSXOpeningElement attributes property (array)
         for(let i = 0; i < nestedJsxElements.length; i++) {
           const innerNest = nestedJsxElements[i].openingElement.attributes;
@@ -171,12 +175,16 @@ export class Parser {
             // if name === onClick, match with value that is JSXExpressionContainer.expression.body.callee 
             if (innerNest[j].value.expression !== undefined) {
               // console.log('passed undefined conditional');
-              if (innerNest[j].value.expression.body.callee.object.name === "router") //"router"
-              // innerNest[j].value.expression.body.callee.property.name //"push"
-              {
-                console.log('~~~~ENDPOINT~~~~~', innerNest[j].value.expression.body.arguments[0].value); //"/cats"
-                endpoints.push(innerNest[j].value.expression.body.arguments[0].value); //"/cats"
-                // return innerNest[j].value.expression.body.arguments[0].value; //"/cats"
+              // console.log('HIT LINE 177 innernest');
+              if (innerNest[j].value.expression.body !== undefined) {
+
+                if (innerNest[j].value.expression.body.callee.object.name === "router") //"router"
+                // innerNest[j].value.expression.body.callee.property.name //"push"
+                {
+                  console.log('~~~~ENDPOINT~~~~~', innerNest[j].value.expression.body.arguments[0].value); //"/cats"
+                  endpoints.push(innerNest[j].value.expression.body.arguments[0].value); //"/cats"
+                  // return innerNest[j].value.expression.body.arguments[0].value; //"/cats"
+                }
               }
             }
           }
@@ -186,9 +194,10 @@ export class Parser {
           // find CallExpression.arguments.value (i.e. '/cats')
   }
 
-  getChildrenComponents(jsxNodes: Array<Node>, importNodes: Array<Node>) {
+  getChildrenComponents(jsxNodes: Array<Node>, importNodes: Array<Node>, nestedPath: string) {
     // console.log('more testing... ', jsxNodes, importNodes);
     const cache = this.mapComponentToFilepath(jsxNodes, importNodes);
+    console.log('cache: ', JSON.stringify(cache));
     const components = [];
     // TODO: handle cases where router variable is not named router 
     
@@ -198,29 +207,42 @@ export class Parser {
     const cacheKeys = Object.keys(cache);
     // console.log('before for loop');
     for (let i = 0; i < cacheKeys.length; i++) {
-      console.log('Cache: ', cache);
+      console.log('Looping over: V', cacheKeys[i]);
+      // splash component is at cacheKey[2];
+      // console.log('Cache: ', cache);
       const filePath = cache[cacheKeys[i]];
-      console.log(filePath.slice(0, 4) !== 'next' && filePath.slice(filePath.length - cacheKeys[i].length) === cacheKeys[i]);
+      // console.log(filePath.slice(0, 4) !== 'next' && filePath.slice(filePath.length - cacheKeys[i].length) === cacheKeys[i]);
       if (filePath.slice(0, 4) !== 'next' && filePath.slice(filePath.length - cacheKeys[i].length) === cacheKeys[i]) {
         //TODO: instead of hardcoding react, maybe pass in fileToRecurse into getChildrenComponents to catch more ed
-
-        console.log('inside double conditional if');
+        if (filePath !== 'react') {
+        // console.log('inside double conditional if');
         // console.log('THE TEST STR: ', this.string);
         // -> /home/nicoflo/cats-app/pages/index.js
         let str = this.string.split('/pages')[0] + filePath.slice(2);
-        console.log(this.string);
-        console.log('string split: ', this.string.split('/pages')[0]);
-        console.log('filePath: ', filePath);
-        console.log('resultantStr: ', str);
+        // console.log(this.string);
+        // console.log('string split: ', this.string.split('/pages')[0]);
+        // console.log('filePath: ', filePath);
+        // console.log('resultantStr: ', str);
         // /home/nicoflo/cats-app + /components/Nav/Jumbotron/Jumbotron
         // get all file paths, match name without extension (.ts, .js, .jsx)
         const extensions = ['.ts', '.js', '.jsx', '.tsx'];
         for (let j = 0; j < extensions.length; j++) {
-          const path = str + extensions[j];
-          // console.log('in J loop this: ', this);
-          if (fs.existsSync(path)) {
-            console.log('in fsExistsSync: ', path);
-            const tree = this.getTree(path);
+          let path = str + extensions[j];
+          console.log('in J loop: ', path);
+          // TAKE PATH, strip all ../ 
+          const arr = path.split('/'); // ['..', '..', 'components', 'Cards' 'CardItem.js']
+          // const newPath = arr.reduce((acc, str) => {
+            //   if (str !== '..') {
+              //     return acc += `/${str}`;
+              //   }
+              // }, '');
+          const newPath = arr.filter((str) => str !== '..').join('/');
+          console.log('NEW PATH', newPath);
+
+          // change 4 paths to newPaths
+          if (fs.existsSync(newPath)) {
+            console.log('in fsExistsSync: ', newPath);
+            const tree = this.getTree(newPath);
             // console.log('*********TREE: ', tree);
             
             // check if current component imports useRouter from next/router
@@ -232,39 +254,70 @@ export class Parser {
               }
             }
             
+            console.log(`in j loop BEFORE ROUTER CHECK: ${cacheKeys[i]}`, newPath);
+            console.log('usesRouter:', usesRouter);
             if (usesRouter) {
               // if conditional to check router vs nonrouter using tree's import nodes
               const endpoints = this.getRouterEndpoints(tree); // '/cats'      console.log('endpoint: ', endpoint);
               // with endpoint, use this.string to find /pages/cats/index.js
               // console.log('after endpoints: ', endpoints);
               // loop over endpoints 
-
-              for (let i = 0; i < endpoints.length; i++) {
-                let fileToRecurse = this.string.split('/pages')[0] + `/pages${endpoints[i]}/index.js`;
+              const endpointChildren = [];
+              const component = new ComponentNode(cacheKeys[i], {}, endpointChildren, 'ssg');
+              for (let k = 0; k < endpoints.length; k++) {
+                console.log('in k loop at 250');
+                let fileToRecurse = this.string.split('/pages')[0] + `/pages${endpoints[k]}/index.js`;
                 // console.log(this.getTree(fileToRecurse));
                 // console.log('fileToRecurse: ', fileToRecurse);
                 const children = [this.recurse(fileToRecurse)];
-                const componentNode = new ComponentNode(cacheKeys[i], {}, children, 'ssg');
-                components.push(componentNode);
+                                                        // cacheKey[0] -> Home
+                                                        // cacheKey[1] -> Nav
+                                                        // splash component is at cacheKey[2];
+                const componentNode = new ComponentNode(endpoints[k], {}, children, 'ssg');
+                component.children.push(componentNode);
               }
-              
+              components.push(component);
+              usesRouter = false;
               // TODO: get props
-              // TODO: figure out recursion over new index.js
-              // TODO: handle component like Nav that doesn't have useRouter
               // recurse over new cats/index.js
               // store as newChildren
               // make new ComponentNode to store information for frontend
               // cacheKeys[i] for name 
               // e.g. {name: 'Jumbotron', props: {}, children: newChildren }
               // newChildren: [{name: '/cats', props: {}, children: [ComponentNode(Nav), ComponentNode(Card)]}]
-              } // if conditional usesRouter
-            } else { 
-              // console.log('usesRouter is falsy: line 255 else statement ');
-              // if conditonal fs.ExistsSync 
-              // get actual components
-            }
+              } else { // if conditional usesRouter
+                console.log('usesRouter is falsy: line 255 else statement:-- ', cacheKeys[i]);
+                const componentNode = new ComponentNode(cacheKeys[i], {}, [], 'ssg');
+                components.push(componentNode);              
+              }
+            } // end if fsExistsSync
           } // end j loop 
-         // if conditional
+        } else { // if react conditional
+          const tree = this.getTree(nestedPath);
+          // console.log('parsing the super nested endpoints in pages/jams/index: ', tree);
+          const endpoints = this.getRouterEndpoints(tree); // '/cats'      console.log('endpoint: ', endpoint);
+          // with endpoint, use this.string to find /pages/cats/index.js
+          // console.log('after endpoints: ', endpoints);
+          // loop over endpoints 
+          // console.log('super nested endpoints: ', endpoints);
+          if (endpoints.length) {
+            for (let i = 0; i < endpoints.length; i++) {
+              console.log('in other loop at 287');
+              let fileToRecurse = this.string.split('/pages')[0] + `/pages${endpoints[i]}/index.js`;
+              // console.log(this.getTree(fileToRecurse));
+              // console.log('fileToRecurse: ', fileToRecurse);
+              const children = [];
+              children.push(this.recurse(fileToRecurse));
+              const componentNode = new ComponentNode(endpoints[i], {}, children, 'ssg');
+              components.push(componentNode);
+            }
+          } 
+          // else {
+          //   const componentNode = new ComponentNode(cacheKeys[i], {}, [], 'ssg');
+          //   components.push(componentNode);   
+          // }
+          console.log('~!@~!@FINAL COMPONENTS~!@~@', components);
+        }
       }
     } // end i loop
     return components;
@@ -282,8 +335,8 @@ export class Parser {
     const childrenNodes = this.getChildrenNodes(exportDefaultNodes);
     const jsxNodes = this.getJsxNodes(childrenNodes); //Head, Nav, Jumbotron
     // console.log('IN RECURSE JSXNodes: ', jsxNodes);
-    console.log('from recurse: ', filePath);
-    const result = this.getChildrenComponents(jsxNodes, importNodes);
+    // console.log('from recurse: ', filePath);
+    const result = this.getChildrenComponents(jsxNodes, importNodes, filePath);
     // console.log('IN RECURSE result: ', result);
     return result;
   }
@@ -305,10 +358,17 @@ export class Parser {
     let exportDefaultNodes;
     exportDefaultNodes = this.getExportDefaultNodes(this.programBody);
     const childrenNodes = this.getChildrenNodes(exportDefaultNodes);
-    const jsxNodes = this.getJsxNodes(childrenNodes); //Head, Nav, Jumbotron
+    const jsxNodes = this.getJsxNodes(childrenNodes); //Head, Nav, Splash
     // console.log('JSXNodes: ', jsxNodes);
     const result = this.getChildrenComponents(jsxNodes, importNodes);
-
+    console.log('****MAIN RESULT*****', result);
+    // Splash
+    //  /jams
+    //    /jams/classics
+    //    /jams/conserves
+    //    /jams/marmalades
+    //  /instruments ...
+    //  /eventss ...
     // Look at each element in array
     // check if there is an associated file with that component name
     // e.g. next/head vs ../components/jumbotron
